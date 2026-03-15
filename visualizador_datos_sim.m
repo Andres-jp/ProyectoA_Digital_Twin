4ce8a14e8875d555956e278e82618893c8caf36f
AndresJP <alu0101416069@ull.edu.es>
2026-03-15 22:16:33 +0000

    Added feature for trajectory visualization

---
+++ visualizador_datos_sim.m
@@ -0,0 +1,35 @@
+numRobots = size(out.BicyclePose, 2) / 3;
+thetaIdx = 3;
+
+% Translation
+xyz = out.BicyclePose;
+xyz(:, thetaIdx) = 0;
+
+% Rotation in XYZ euler angles
+theta = out.BicyclePose(:,thetaIdx);
+thetaEuler = zeros(size(out.BicyclePose, 1), 3 * size(theta, 2));
+thetaEuler(:, end) = theta;
+
+for k = 1:size(xyz, 1)
+    show(map)
+    hold on;
+    
+    % Plot Start Location
+    plotTransforms([startLoc, 0], eul2quat([0, 0, 0]))
+    text(startLoc(1), startLoc(2), 2, 'Start');
+    
+    % Plot Goal Location
+    plotTransforms([goalLoc, 0], eul2quat([0, 0, 0]))
+    text(goalLoc(1), goalLoc(2), 2, 'Goal');
+    
+    % Plot Robot's XY locations
+    plot(out.BicyclePose(:, 1), out.BicyclePose(:, 2), '-b')
+    
+    % Plot Robot's pose as it traverses the path
+    quat = eul2quat(thetaEuler(k, :), 'xyz');
+    plotTransforms(xyz(k,:), quat, 'MeshFilePath',...
+        'groundvehicle.stl');
+    
+    pause(0.0001)
+    hold off;
+end
