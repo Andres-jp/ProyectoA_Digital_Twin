616cb2aa9e8f4bb38bec62c2f807f02555b192dc
AndresJP <alu0101416069@ull.edu.es>
2026-03-16 19:46:28 +0000

    Fixed variables names in Data Inspector.

--- visualizador_datos_sim.m
+++ visualizador_datos_sim.m
@@ -1,14 +1,22 @@
-numRobots = size(out.BicyclePose, 2) / 3;
+% 1. Extraer los arreglos numéricos de los objetos timeseries
+x_data = out.BicyclePose.x.Data(:);
+y_data = out.BicyclePose.y.Data(:);
+theta_data = out.BicyclePose.theta.Data(:);
+
+% 2. Reconstruir la matriz original [X, Y, Theta]
+poseMatrix = [x_data, y_data, theta_data];
+
+numRobots = size(poseMatrix, 2) / 3;
 thetaIdx = 3;
-map = binaryOccupancyMap(simpleMap)
+map = binaryOccupancyMap(simpleMap);
 
 % Translation
-xyz = out.BicyclePose;
-xyz(:, thetaIdx) = 0;
+xyz = poseMatrix;
+xyz(:, thetaIdx) = 0; 
 
 % Rotation in XYZ euler angles
-theta = out.BicyclePose(:,thetaIdx);
-thetaEuler = zeros(size(out.BicyclePose, 1), 3 * size(theta, 2));
+theta = poseMatrix(:, thetaIdx);
+thetaEuler = zeros(size(poseMatrix, 1), 3 * size(theta, 2));
 thetaEuler(:, end) = theta;
 
 for k = 1:size(xyz, 1)
@@ -24,12 +32,11 @@ for k = 1:size(xyz, 1)
     text(goalLoc(1), goalLoc(2), 2, 'Goal');
     
     % Plot Robot's XY locations
-    plot(out.BicyclePose(:, 1), out.BicyclePose(:, 2), '-b')
+    plot(poseMatrix(:, 1), poseMatrix(:, 2), '-b')
     
     % Plot Robot's pose as it traverses the path
     quat = eul2quat(thetaEuler(k, :), 'xyz');
-    plotTransforms(xyz(k,:), quat, 'MeshFilePath',...
-        'groundvehicle.stl');
+    plotTransforms(xyz(k,:), quat, 'MeshFilePath', 'groundvehicle.stl');
     
     pause(0.0001)
     hold off;
